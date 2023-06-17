c39beff34e21f19a77681a4d0a39e9fe3e579e1b
Dat T. Tran <datttrian@gmail.com>
2023-06-17 15:39:49 -0400

    Add card detail modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CustomFilter, Hero, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
 
 import { fetchCars } from '@/utils';
 
@@ -22,6 +22,24 @@ export default async function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ): (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">
+              Oops, no results
+            </h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )
+      }
       </div>
     </main>
   )
