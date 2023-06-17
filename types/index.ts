e7f83d5e1cdd3ef293e635a6f155346b2da18767
Dat T. Tran <datttrian@gmail.com>
2023-06-17 17:02:11 -0400

    Add search functionality

--- types/index.ts
+++ types/index.ts
@@ -12,11 +12,17 @@ export interface CustomButtonProps {
 
 export interface SearchManuFacturerProps {
     manufacturer: string;
-    setManufacturer: (manufacturer: string) => void;
+    setManuFacturer: (manufacturer: string) => void;
+  }
+
+export interface OptionProps {
+  title: string;
+  value: string;
 }
 
 export interface CustomFilterProps {
     title: string;
+    options: OptionProps[];
 }
 
 export interface CarProps {
@@ -33,3 +39,15 @@ export interface CarProps {
     transmission:string;
     year:number;
 }
+
+export interface FilterProps {
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
+}
+
+export interface HomeProps {
+    searchParams: FilterProps;
+  }
